31937787fc10311fac0e81109bc2578209581323
Marcelo Avanzi <marcelodaniel@hotmail.com>
2021-09-29 18:15:55 -0300

    Feito até o cap 12

---
+++ ExdoLivro.py
@@ -0,0 +1,428 @@
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import Grouper
+from pandas import concat
+from pandas.plotting import lag_plot
+from matplotlib import pyplot
+from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa.seasonal import seasonal_decompose
+from random import randrange
+from sklearn.linear_model import LinearRegression
+import numpy
+import datetime
+from numpy import polyfit
+from statsmodels.tsa.stattools import adfuller
+from sklearn.model_selection import TimeSeriesSplit
+
+series = read_csv('teste.csv', sep=';', header=0, index_col=0, parse_dates=True, squeeze=True)
+#print(series['2018-01'])
+
+
+#modelo Naive
+'''
+dataframe = DataFrame()
+dataframe['year'] = [series.index[i].year for i in range(len(series))]
+dataframe['month'] = [series.index[i].month for i in range(len(series))]
+dataframe['day'] = [series.index[i].day for i in range(len(series))]
+dataframe['vazao'] = [series[i] for i in range(len(series))]
+print(dataframe.head(5))
+'''
+
+
+#Lag de t+1
+'''
+temps = DataFrame(series.values)
+dataframe = concat([temps.shift(1), temps], axis=1)
+dataframe.columns = ['t', 't+1']
+print(dataframe.head(5))
+'''
+
+#Lag de t-2 t-1 e t+1
+'''
+temps = DataFrame(series.values)
+dataframe = concat([temps.shift(3), temps.shift(2), temps.shift(1), temps], axis=1)
+dataframe.columns = ['t-2', 't-1', 't', 't+1']
+print(dataframe.head(5))
+'''
+
+#Rolling mean
+'''
+temps = DataFrame(series.values)
+shifted = temps.shift(1)
+window = shifted.rolling(window=2)
+means = window.mean()
+dataframe = concat([means, temps], axis=1)
+dataframe.columns = ['mean(t-1,t)', 't+1']
+print(dataframe.head(5))
+'''
+
+#Rolling mean com 5
+'''
+temps = DataFrame(series.values)
+width = 3
+shifted = temps.shift(width - 1)
+window = shifted.rolling(window=width)
+dataframe = concat([window.min(), window.mean(), window.max(), temps], axis=1)
+dataframe.columns = ['min', 'mean', 'max', 't+1']
+print(dataframe.head(5))
+'''
+
+#Expanding window
+'''
+temps = DataFrame(series.values)
+window = temps.expanding()
+dataframe = concat([window.min(), window.mean(), window.max(), temps.shift(-1)], axis=1)
+dataframe.columns = ['min', 'mean', 'max', 't+1']
+print(dataframe.head(5))
+'''
+
+#Line Plot or dot plot
+'''
+series.plot(style='k.')
+pyplot.show()
+'''
+
+#Comparing plots by year
+'''
+groups = series.groupby(Grouper(freq='A'))
+years = DataFrame()
+for name, group in groups:
+    years[name.year] = group.values
+years.plot(subplots=True, legend=True)
+pyplot.show()
+'''
+
+#Plotando histograma
+'''
+series.hist()
+pyplot.show()
+'''
+
+# Plotando para saber a densidade
+'''
+series.plot(kind='kde')
+pyplot.show()
+'''
+
+#Plot de box e whisker
+'''
+groups = series.groupby(Grouper(freq='A'))
+years = DataFrame()
+for name, group in groups:
+    years[name.year] = group.values
+years.boxplot()
+pyplot.show()
+'''
+
+#Plot de box e whisker separado por mes em 2018
+'''
+one_year = series['2018']
+groups = one_year.groupby(Grouper(freq='M'))
+months = concat([DataFrame(x[1].values) for x in groups], axis=1)
+months = DataFrame(months)
+months.columns = range(1,13)
+months.boxplot()
+pyplot.show()
+'''
+
+#heat map
+'''
+groups = series.groupby(Grouper(freq='A'))
+years = DataFrame()
+for name, group in groups:
+    years[name.year] = group.values
+years = years.T
+pyplot.matshow(years, interpolation=None, aspect='auto')
+pyplot.show()
+'''
+
+#heat map separado por mes em 2018
+'''
+one_year = series['2018']
+groups = one_year.groupby(Grouper(freq='M'))
+months = concat([DataFrame(x[1].values) for x in groups], axis=1)
+months = DataFrame(months)
+months.columns = range(1,13)
+pyplot.matshow(months, interpolation=None, aspect='auto')
+pyplot.show()
+'''
+
+#lag plot
+'''
+lag_plot(series)
+pyplot.show()
+'''
+
+#lag plot comparando com os dias atras
+'''
+values = DataFrame(series.values)
+lags = 7
+columns = [values]
+for i in range(1,(lags + 1)):
+    columns.append(values.shift(i))
+dataframe = concat(columns, axis=1)
+columns = ['t']
+for i in range(1,(lags + 1)):
+    columns.append('t-' + str(i))
+dataframe.columns = columns
+pyplot.figure(1)
+for i in range(1, (lags + 1)):
+    ax = pyplot.subplot(240 + i)
+    ax.set_title('t vs t-' + str(i))
+    pyplot.scatter(x=dataframe['t'].values, y=dataframe['t-' + str(i)].values)
+pyplot.show()
+'''
+
+#auto correlação entre observação e lag
+'''
+autocorrelation_plot(series)
+pyplot.show()
+'''
+
+#decomposição em sazonalidade tendencia e residuo etc
+'''
+result = seasonal_decompose(series, model='multiplicative',freq=3)
+result.plot()
+pyplot.show()
+'''
+
+#removendo tendencias
+'''
+def parser(x):
+    return datetime.strptime('190'+x, '%Y-%m')
+series = read_csv('teste.csv', sep=';', header=0, index_col=0, parse_dates=True, squeeze=True)
+X = series.values
+diff = list()
+for i in range(1, len(X)):
+    value = X[i] - X[i - 1]
+    diff.append(value)
+pyplot.plot(diff)
+pyplot.show()
+'''
+
+#Removendo tendencia com model fitting
+'''
+def parser(x):
+    return datetime.strptime('190'+x, '%Y-%m')
+series = read_csv('teste.csv', sep=';', header=0, index_col=0, parse_dates=True, squeeze=True, date_parser=parser)
+# fit linear model
+X = [i for i in range(0, len(series))]
+X = numpy.reshape(X, (len(X), 1))
+y = series.values
+model = LinearRegression()
+model.fit(X, y)
+# calculate trend
+trend = model.predict(X)
+# plot trend
+pyplot.plot(y)
+pyplot.plot(trend)
+pyplot.show()
+# detrend
+detrended = [y[i]-trend[i] for i in range(0, len(series))]
+# plot detrended
+pyplot.plot(detrended)
+pyplot.show()
+'''
+
+#removendo sazonalidade
+'''
+X = series.values
+diff = list()
+days_in_year = 365
+for i in range(days_in_year, len(X)):
+    value = X[i] - X[i - days_in_year]
+    diff.append(value)
+pyplot.plot(diff)
+pyplot.show()
+'''
+
+#Calcula e plota media mensal
+'''
+resample = series.resample('M')
+monthly_mean = resample.mean()
+print(monthly_mean.head(13))
+monthly_mean.plot()
+pyplot.show()
+print(series)
+'''
+
+#cria a sazonalidade a partir da media mensal
+'''
+resample = series.resample('M')
+monthly_mean = resample.mean()
+X = series.values
+diff = list()
+months_in_year = 12
+for i in range(months_in_year, len(monthly_mean)):
+    value = monthly_mean[i] - monthly_mean[i - months_in_year]
+    diff.append(value)
+pyplot.plot(diff)
+pyplot.show()
+'''
+
+
+#cria a sazonalidade a partir da media mensal mais robusto
+'''
+X = series.values
+diff = list()
+days_in_year = 365
+for i in range(days_in_year, len(X)):
+    month_str = str(series.index[i].year-1)+'-'+str(series.index[i].month)
+    month_mean_last_year = series[month_str].mean()
+    value = X[i] - month_mean_last_year
+    diff.append(value)
+pyplot.plot(diff)
+pyplot.show()
+'''
+
+#cria um model seasonality with a polynomial model
+'''
+# fit polynomial: x^2*b1 + x*b2 + ... + bn
+X = [i%365 for i in range(0, len(series))]
+y = series.values
+degree = 4
+coef = polyfit(X, y, degree)
+print('Coefficients: %s' % coef)
+# create curve
+curve = list()
+for i in range(len(X)):
+    value = coef[-1]
+    for d in range(degree):
+        value += X[i]**(degree-d) * coef[d]
+    curve.append(value)
+# plot curve over original data
+pyplot.plot(series.values)
+pyplot.plot(curve, color='red', linewidth=3)
+pyplot.show()
+'''
+
+#deseasonalize by differencing with a polynomial model
+'''
+# fit polynomial: x^2*b1 + x*b2 + ... + bn
+X = [i%365 for i in range(0, len(series))]
+y = series.values
+degree = 4
+coef = polyfit(X, y, degree)
+# create curve
+curve = list()
+for i in range(len(X)):
+    value = coef[-1]
+    for d in range(degree):
+        value += X[i]**(degree-d) * coef[d]
+    curve.append(value)
+# create seasonally adjusted
+values = series.values
+diff = list()
+for i in range(len(values)):
+    value = values[i] - curve[i]
+    diff.append(value)
+pyplot.plot(diff)
+pyplot.show()
+'''
+
+#Verificar se é estacionaria usando histograma e media das metades da serie
+# se a media e a variancia dos dois lados sao parecidas entoa é estacionario
+'''
+series.hist()
+X = series.values
+split = int(len(X) / 2)
+X1, X2 = X[0:split], X[split:]
+mean1, mean2 = X1.mean(), X2.mean()
+var1, var2 = X1.var(), X2.var()
+print('mean1=%f, mean2=%f' % (mean1, mean2))
+print('variance1=%f, variance2=%f' % (var1, var2))
+pyplot.show()
+'''
+
+#calculate stationarity test of time series data
+'''
+X = series.values
+result = adfuller(X)
+print('ADF Statistic: %f' % result[0])
+print('p-value: %f' % result[1])
+print('Critical Values:')
+for key, value in result[4].items():
+    print('\t%s: %.3f' % (key, value))
+'''
+
+# calculate a train-test split of a time series dataset de 66% pra treino e 34% pra teste
+'''
+X = series.values
+train_size = int(len(X) * 0.66)
+train, test = X[0:train_size], X[train_size:len(X)]
+print('Observations: %d' % (len(X)))
+print('Training Observations: %d' % (len(train)))
+print('Testing Observations: %d' % (len(test)))
+pyplot.plot(train)
+pyplot.plot([None for i in train] + [x for x in test])
+pyplot.show()
+'''
+
+#calculate repeated train-test splits of time series data
+'''
+X = series.values
+splits = TimeSeriesSplit(n_splits=3)
+pyplot.figure(1)
+index = 1
+for train_index, test_index in splits.split(X):
+    train = X[train_index]
+    test = X[test_index]
+    print('Observations: %d' % (len(train) + len(test)))
+    print('Training Observations: %d' % (len(train)))
+    print('Testing Observations: %d' % (len(test)))
+    pyplot.subplot(310 + index)
+    pyplot.plot(train)
+    pyplot.plot([None for i in train] + [x for x in test])
+    index += 1
+pyplot.show()
+'''
+
+# walk forward evaluation model for time series data (walk forward validation method)
+'''
+X = series.values
+n_train = 500
+n_records = len(X)
+for i in range(n_train, n_records):
+    train, test = X[0:i], X[i:i+1]
+    print('train=%d, test=%d' % (len(train), len(test)))
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
